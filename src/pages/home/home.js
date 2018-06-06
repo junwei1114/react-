a03d941b11999e8407f4802019a5533f145e40ff
junwei1114 <wwww7314>
2018-06-06 10:04:03 +0800

    补上less引用

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,33 +1,92 @@
-import React, { Component } from 'react';
+
+import { TabBar } from 'antd-mobile';
+import React from 'react';
 import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
 // import API from '../../api/api';
 import './home.less';
 
 
 
-class Home extends Component {
-    static propTypes = {
-        // formData: PropTypes.object.isRequired,
-        // saveFormData: PropTypes.func.isRequired,
-        // saveImg: PropTypes.func.isRequired,
-        // clearData: PropTypes.func.isRequired,
-        // clearSelected: PropTypes.func.isRequired,
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'redTab',
+            hidden: false,
+            fullScreen: false,
+        };
     }
 
-    state = {
-        alertStatus: false, //弹框状态
-        alertTip: '', //弹框提示文字
-    }
-
-    componentWillMount(){
-
+    renderContent(pageText) {
+        return (
+            <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
+                <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
+                <a style={{ display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9' }}
+                   onClick={(e) => {
+                       e.preventDefault();
+                       this.setState({
+                           hidden: !this.state.hidden,
+                       });
+                   }}
+                >
+                    Click to show/hide tab-bar
+                </a>
+            </div>
+        );
     }
 
     render() {
         return (
-            <div>主页</div>
-        )
+            <div style={this.state.fullScreen ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
+                <TabBar
+                    unselectedTintColor="#949494"
+                    tintColor="#33A3F4"
+                    barTintColor="white"
+                    hidden={this.state.hidden}
+                >
+                    <TabBar.Item
+                        title="Life"
+                        key="Life"
+                        icon={<div style={{
+                            width: '22px',
+                            height: '22px',
+                            background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+                        />
+                        }
+                        selectedIcon={<div style={{
+                            width: '22px',
+                            height: '22px',
+                            background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+                        />
+                        }
+                        selected={this.state.selectedTab === 'blueTab'}
+                        badge={1}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'blueTab',
+                            });
+                        }}
+                        data-seed="logId"
+                    >
+                        {this.renderContent('Life')}
+                    </TabBar.Item>
+                    <TabBar.Item
+                        icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+                        selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+                        title="My"
+                        key="my"
+                        selected={this.state.selectedTab === 'yellowTab'}
+                        onPress={() => {
+                            this.setState({
+                                selectedTab: 'yellowTab',
+                            });
+                        }}
+                    >
+                        {this.renderContent('My')}
+                    </TabBar.Item>
+                </TabBar>
+            </div>
+        );
     }
 }
 
